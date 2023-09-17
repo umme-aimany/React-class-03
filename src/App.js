496e472e81e7de6e174c0811c405b397686da782
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2023-09-17 12:42:52 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import './App.css';
+import Card from './Components/UI/Card';
+
+
+function App() {
+  return (
+    <Card />
+  )
+}
+export default App;
---
+++ src/Components/UI/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Card.css';
+
+export default function Card ()  {
+  return (
+   
+    <div className='card'>
+    Card Component</div>
+ 
+  )
+}
